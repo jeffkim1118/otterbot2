edc58c88bace376b99ff3b29d48e7c97e61e5a72
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2022-10-06 00:33:04 -0400

    Updated otterbot v2 with send message when a new user joins the server.

--- otterbotv2.py
+++ otterbotv2.py
@@ -3,13 +3,26 @@ import discord
 intents = discord.Intents.default()
 intents.message_content = True
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
+TOKEN = "TOKEN HERE"
 
-    async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
+client = discord.Client(intents=intents)
 
-
-client = MyClient(intents=intents)
-client.run('Client key goes here!')
+@client.event
+async def on_ready():
+        print(f'{client.user} has connected to Discord!')
+        
+        
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to CaptainOtterz discord server~! / {member.name}님, 캡틴오터의 디코서버에 오신걸 환영합니다~!'
+    )
+    
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('+hello'):
+        await message.channel.send(f'Hello {message.author.name}! / 안녕하세요! {message.author.name}님!')
+client.run(TOKEN)
